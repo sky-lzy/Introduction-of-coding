2d0d6aa25bc7bb85e007a16b3e459539f63b3c14
sky-lzy <289751894@qq.com>
2022-10-09 13:41:26 +0800

    hw1_1 visualize: differet scenarios

--- hw1_1/digital_channel.m
+++ hw1_1/digital_channel.m
@@ -1,5 +1,8 @@
 function output_bits = digital_channel(input_bits, N_bits, T, b, rou, sigma_n, Is_plot_scatter, Send_acquire_beta, Recv_acquire_beta)
 
+    send_flag = exist('Send_acquire_beta', 'var') && (Send_acquire_beta == true);
+    recv_flag = exist('Recv_acquire_beta', 'var') && (Recv_acquire_beta == true);
+
     % encode: N_bits-bit for every signal
     M = 2^N_bits;
     len_encode = length(input_bits)/N_bits;
@@ -10,10 +13,20 @@ function output_bits = digital_channel(input_bits, N_bits, T, b, rou, sigma_n, I
     
     % generate channel
     a = generate_channel(len_encode*T, b, rou);
+    avg_a = zeros(1, len_encode);
+    for iter_t = 1:T
+        avg_a = mean(a(T*(iter_t-1)+1:T*iter_t));
+    end
 
-    % 
+    % signal processing
     u = pskmod(encode_bits, M);
+    if (send_flag)
+        u = u .* exp(-1j*angle(avg_a));
+    end
     v = complex_elec_channel(u, T, a, sigma_n);
+    if (~send_flag && recv_flag)
+        v = v .* exp(-1j*angle(avg_a));
+    end
     receive_bits = pskdemod(v, M);
 
     % decode
--- hw1_1/visualize_b_rou.m
+++ hw1_1/visualize_b_rou.m
@@ -14,16 +14,20 @@ rng(0); % set random seed
 len_bits = 100;
 N_cycle = 100;
 send_bits = randi([0, 1], 1, len_bits);
+send_case = [false, false, true];
+recv_case = [false, true, true];
 
-errors = zeros(3, N_SNR);
+errors = zeros(3, 3, N_SNR);
 for iter_SNR = 1:N_SNR
 
-    for k = 1:3
-        rng(0);
-        for iter_n = 1:N_cycle
-            receive_bits = digital_channel(send_bits, 1, T, b(k), rou(k), sigma_n(iter_SNR));
-            error_num = sum(abs(send_bits - receive_bits));
-            errors(k, iter_SNR) = errors(k, iter_SNR) + error_num / len_bits;
+    for iter_case = 1:3
+        for k = 1:3
+            rng(0);
+            for iter_n = 1:N_cycle
+                receive_bits = digital_channel(send_bits, 1, T, b(k), rou(k), sigma_n(iter_SNR), false, send_case(iter_case), recv_case(iter_case));
+                error_num = sum(abs(send_bits - receive_bits));
+                errors(iter_case, k, iter_SNR) = errors(iter_case, k, iter_SNR) + error_num / len_bits;
+            end
         end
     end
 
@@ -31,14 +35,51 @@ end
 errors = errors / N_cycle;
 
 %% visualization
+errors1 = reshape(errors(1, :, :), [3, N_SNR]);
+errors2 = reshape(errors(2, :, :), [3, N_SNR]);
+errors3 = reshape(errors(3, :, :), [3, N_SNR]);
+
 figure(); 
-semilogy(pow2db(SNR), errors(1, :), 'Marker', '*', 'Linewidth', 2); hold on;
-semilogy(pow2db(SNR), errors(2, :), 'Marker', 's', 'Linewidth', 2);
-semilogy(pow2db(SNR), errors(3, :), 'Marker', 'o', 'Linewidth', 2);
+semilogy(pow2db(SNR), errors1(1, :), 'Marker', '*', 'Linewidth', 2); hold on;
+semilogy(pow2db(SNR), errors1(2, :), 'Marker', 's', 'Linewidth', 2);
+semilogy(pow2db(SNR), errors1(3, :), 'Marker', 'o', 'Linewidth', 2);
 legend('b = 0, rou = 0', 'b = 1, rou = 1', 'b = 0.7, rou = 0.996');
 xlabel('SNR (dB)');
 ylabel('Error Rate');
 title('Variation of Error Rate with SNR within Different b and rou');
 set(gca, 'FontName', 'Times New Roman');
 
+figure(); 
+semilogy(pow2db(SNR), errors2(1, :), 'Marker', '*', 'Linewidth', 2); hold on;
+semilogy(pow2db(SNR), errors2(2, :), 'Marker', 's', 'Linewidth', 2);
+semilogy(pow2db(SNR), errors2(3, :), 'Marker', 'o', 'Linewidth', 2);
+legend('b = 0, rou = 0', 'b = 1, rou = 1', 'b = 0.7, rou = 0.996');
+xlabel('SNR (dB)');
+ylabel('Error Rate');
+title('Variation of Error Rate with SNR within Different b and rou (With known betas in recv)');
+set(gca, 'FontName', 'Times New Roman');
+
+figure(); 
+semilogy(pow2db(SNR), errors3(1, :), 'Marker', '*', 'Linewidth', 2); hold on;
+semilogy(pow2db(SNR), errors3(2, :), 'Marker', 's', 'Linewidth', 2);
+semilogy(pow2db(SNR), errors3(3, :), 'Marker', 'o', 'Linewidth', 2);
+legend('b = 0, rou = 0', 'b = 1, rou = 1', 'b = 0.7, rou = 0.996');
+xlabel('SNR (dB)');
+ylabel('Error Rate');
+title('Variation of Error Rate with SNR within Different b and rou (With known betas in both send and recv)');
+set(gca, 'FontName', 'Times New Roman');
+
+figure();
+semilogy(pow2db(SNR), errors1(1, :), 'LineStyle', '-', 'Marker', 'o', 'Linewidth', 2); hold on;
+semilogy(pow2db(SNR), errors1(2, :), 'LineStyle', '-', 'Marker', '+', 'Linewidth', 2);
+semilogy(pow2db(SNR), errors1(3, :), 'LineStyle', '-', 'Marker', 'x', 'Linewidth', 2);
+semilogy(pow2db(SNR), errors2(1, :), 'Linestyle', '--', 'Marker', 'o', 'Linewidth', 2);
+semilogy(pow2db(SNR), errors2(2, :), 'Linestyle', '--', 'Marker', '+', 'Linewidth', 2);
+semilogy(pow2db(SNR), errors2(3, :), 'Linestyle', '--', 'Marker', 'x', 'Linewidth', 2);
+semilogy(pow2db(SNR), errors3(1, :), 'Linestyle', ':', 'Marker', 'o', 'Linewidth', 2);
+semilogy(pow2db(SNR), errors3(2, :), 'Linestyle', ':', 'Marker', '+', 'Linewidth', 2);
+semilogy(pow2db(SNR), errors3(3, :), 'Linestyle', ':', 'Marker', 'x', 'Linewidth', 2);
+legend('b = 0, rou = 0, no beta', 'b = 1, rou = 1, no beta', 'b = 0.7, rou = 0.996, no beta', 'b = 0, rou = 0, recv beta', 'b = 1, rou = 1, recv beta', 'b = 0.7, rou = 0.996, recv beta', 'b = 0, rou = 0, send and recv beta', 'b = 1, rou = 1, send and recv beta', 'b = 0.7, rou = 0.996, send and recv beta');
+xlabel('SNR (dB)');
+ylabel('Error Rate');
 
