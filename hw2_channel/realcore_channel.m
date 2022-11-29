2da27aedc9868c18d2a53ca688d1d63ec28c75c4
wang zheng <wzheng20@mails.tsinghua.edu.cn>
2022-11-29 22:07:06 +0800

    update

--- hw2_channel/realcore_channel.m
+++ hw2_channel/realcore_channel.m
@@ -1,12 +1,29 @@
-function [output,eb] = realcore_channel(input,N_bits,T,K,fs,n0,visualize)
-    u = pskmod(input,2^N_bits);% 将比特映射为复电平
-%     scatterplot(u);
-    [v,es] = core(u,T,K,fs,n0,visualize);
-    eb = es/N_bits;
-    output = pskdemod(v,N_bits);%符号判决
+function [output_code,es] = realcore_channel(input_code,N_bits,T,K,fs,n0,visualize)
+%   输出判决结果和总符号能量
+    len_encode = length(input_code);
+    encode_code = zeros(1, len_encode/N_bits);
+    for iter = 1:len_encode/N_bits
+        encode_code(iter) = bin2dec(num2str(input_code(N_bits*(iter-1)+1:N_bits*iter)));
+    end
+    u = pskmod(encode_code,2^N_bits);% 将比特映射为复电平
+    
+    [v,es,u,x_plot,s_plot,r_plot] = core(u,T,K,fs,n0);
+    
+    receive_code = pskdemod(v,2^N_bits);%符号判决
+    output_code = zeros(1, length(input_code));
+    for iter = 1:len_encode/N_bits
+        output_code(N_bits*(iter-1)+1:N_bits*iter) = dec2bin(receive_code(iter), N_bits) - '0';
+    end
+    
+    if visualize
+        visual(fs,T,K,input_code,u,v,x_plot,s_plot,r_plot);
+    end
 end
 
-function [v,es] = core(u,T,K,fs,n0,visualize)
+function [v,es,u,x_plot,s_plot,r_plot] = core(u,T,K,fs,n0)
+    % 内核为实采样的复电平信道
+    % 事实上只有输出复电平v和总符号能量es有用，别的参数用来画图
+    % fs用作理论模拟，不影响信道传输
     rng(0);
     
     N = length(u); % 符号序列长度
@@ -26,13 +43,15 @@ function [v,es] = core(u,T,K,fs,n0,visualize)
         noise = randn(1,T)*sqrt(fs*n0/2);
         r = s+noise;
         r_plot = [r_plot,r];
-        y = real(r*2.*carrier.^(-1));
+        y = r*2.*carrier.^(-1);
         v(i) = sum(y)/T;
     end
     es = es/fs;
     
-    
-    if visualize
+end
+
+function visual(fs,T,K,input_code,u,v,x_plot,s_plot,r_plot)
+        N = length(u); % 符号序列长度
         % planisphere
         scatterplot(u);
         scatterplot(v);
@@ -56,18 +75,24 @@ function [v,es] = core(u,T,K,fs,n0,visualize)
         % waveform
         figure();
         subplot(2,1,1);hold on;
-        xlabel('t');
+        xlabel('采样点');
         ylabel('s');
         title('Transmission signal waveform')
         grid on;
-        plot(0:N*T-1,s_plot);
-        
+        % 只画二元符号映射
+        for i = 0:N-1
+            if input_code(i+1)==0
+                plot(i*T:(i+1)*T-1,s_plot(i*T+1:(i+1)*T),'color','r','linewidth',1.5);
+            else
+                plot(i*T:(i+1)*T-1,s_plot(i*T+1:(i+1)*T),'color','g','linewidth',1.5);
+            end
+        end
         subplot(2,1,2);hold on;
-        xlabel('t');
+        xlabel('采样点');
         ylabel('r');
         title('Received signal waveform')
         grid on;
-        plot(0:N*T-1,r_plot);
+        plot(0:N*T-1,r_plot,'linewidth',1.5);
         
         
         
@@ -87,7 +112,4 @@ function [v,es] = core(u,T,K,fs,n0,visualize)
         title('power spectrum of received signal')
         grid on;
         
-        
-        
     end
-end
--- hw2_channel/realcore_channel_test.m
+++ hw2_channel/realcore_channel_test.m
@@ -1,19 +1,19 @@
 %% initial test
-clc;clear;
-N = 100;
+clc;clear;close all;
+N = 5;
 dtest = round(rand(1,N));
 T = 21;
 K = 3;
 fs = 10000;% 假定采样率10k
 n0 = 0.0001;
 sigma2 = fs*n0/2;
-[output,eb] = realcore_channel(dtest,1,T,K,fs,n0,1);
+[output,es] = realcore_channel(dtest,2,T,K,fs,n0,1);
 disp('BER:');
 display(1-sum(output==dtest)/N);
 disp('Eb/n0:');
-display(pow2db(eb/n0));
-%% BER 1/3 convolutional code 
-L = 10000;
+display(pow2db(es/n0/N));
+%% BER 1/3 convolutional code n-bits mapping
+L = 100000;
 g = [1,1,0,1;1,0,1,1;1,1,1,1];
 input = round(rand(1,L));
 T = 21;
@@ -23,18 +23,62 @@ N_dots = 15;
 
 n0 = logspace(-6,0,N_dots);
 sigma2 = fs*n0/2;
-ber = zeros(1,N_dots);
+ber_bit = zeros(3,N_dots);
 Eb = zeros(1,N_dots);
 for i = 1:N_dots
     output_code = conv_encode(input, 3, 0, 0);
-    [output,eb] = realcore_channel(output_code,1,T,K,fs,n0(i),0);
-    [decode,corr_rate] = conv_decode(output',L,g,0,input,0,0,0);
-    ber(i) = 1-corr_rate;
-    Eb(i) = eb;
+    % 1bit mapping
+    [output,es] = realcore_channel(output_code,1,T,K,fs,n0(i),0);
+    [~,corr_rate] = conv_decode(output',L,g,0,input,0,0,0);
+    ber_bit(1,i) = 1-corr_rate;
+    Eb(1,i) = es/L;
+    % 2bit mapping
+    [output,es] = realcore_channel(output_code,2,T,K,fs,n0(i),0);
+    [~,corr_rate] = conv_decode(output',L,g,0,input,0,0,0);
+    ber_bit(2,i) = 1-corr_rate;
+    Eb(2,i) = es/L;
+    % 3bit mapping
+    [output,es] = realcore_channel(output_code,3,T,K,fs,n0(i),0);
+    [~,corr_rate] = conv_decode(output',L,g,0,input,0,0,0);
+    ber_bit(3,i) = 1-corr_rate;
+    Eb(3,i) = es/L;
 end
 figure();
-semilogy(pow2db(Eb./n0), ber, 'Marker', 'o', 'Linewidth', 2); hold on;
+semilogy(pow2db(Eb(1,:)./n0), ber_bit(1,:), 'Marker', 'o', 'Linewidth', 2); hold on;
+semilogy(pow2db(Eb(2,:)./n0), ber_bit(2,:), 'Marker', 'o', 'Linewidth', 2);
+semilogy(pow2db(Eb(3,:)./n0), ber_bit(3,:), 'Marker', 'o', 'Linewidth', 2);
 grid on;
+legend('1-bit mapping','2-bits mapping','3-bits mapping')
 xlabel('Eb/n0(dB)');
 ylabel('BER');
 title('BER');
+%% BER 1/3 convolutional code different symbol rate
+L = 1000;
+g = [1,1,0,1;1,0,1,1;1,1,1,1];
+input = round(rand(1,L));
+T = 21;
+K = 3;
+Rs = [10^3,10^6,10^9];
+len_rs = length(Rs);
+fs = Rs*T;
+N_dots = 15;
+
+n0 = logspace(-6,0,N_dots);
+ber_bit = zeros(1,N_dots);
+Eb = zeros(len_rs,N_dots);
+figure();
+for k = 1:len_rs
+    for i = 1:N_dots
+        output_code = conv_encode(input, 3, 0, 0);
+        % 1bit mapping
+        [output,es] = realcore_channel(output_code,1,T,K,fs(k),n0(i),0);
+        [~,corr_rate] = conv_decode(output',L,g,0,input,0,0,0);
+        ber_bit(k,i) = 1-corr_rate;
+        Eb(k,i) = es/L;
+    end
+    semilogy(pow2db(Eb(k,:)./n0), ber_bit(1,:), 'Marker', 'o', 'Linewidth', 2); hold on;
+    grid on;
+    xlabel('Eb/n0(dB)');
+    ylabel('BER');
+    title('BER');
+end
