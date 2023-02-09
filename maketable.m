24586c009c1bcddc3bdbe368e56b78f78f96eb3f
zyz-2333-huaiqian <zhao-yz20@mails.tsinghua.edu.cn>
2023-02-09 21:19:37 +0800

    table

--- maketable.m
+++ maketable.m
@@ -1,9 +1,19 @@
-symbols = 10:20:250;
-prob = zeros(size(symbols));
-prob = prob+1/length(symbols);
-dict1 = huffmandict(symbols,prob);
+function bitnum = maketable(prob,nodes)
+    prob_norm = prob./sum(prob);
+    prob_norm = [prob_norm,0];
+    symbols = (0:nodes);
+    [dict,avglen] = huffmandict(symbols,prob_norm);
+    s = size(picture);
+    bitnum = s(1)*s(2)*avglen;
 
-symbols = 1:6;
-prob = zeros(size(symbols));
-prob = prob+1/length(symbols);
-dict2 = huffmandict(symbols,prob);
+    fid = fopen('table.txt', 'wt');
+    for m = 1:size(mat1,1)
+        fprintf(fid, '%d',cell2mat(dict(m,1)));
+        fprintf(fid, ' ');
+        fprintf(fid, '%s',strrep(num2str(cell2mat(dict(m,2))),' ',''));
+        fprintf(fid, '\n');
+    end
+    fclose(fid);
+
+
+end
